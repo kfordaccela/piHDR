eda7e80802a28671123730e0c5ad627e677b11f8
royord <royord@gmail.com>
2019-04-11 13:47:38 -0700

    value for shutter speed must be an INT

--- exposurebracket.py
+++ exposurebracket.py
@@ -20,21 +20,24 @@ with picamera.PiCamera() as camera:
     camera.awb_gains = (1.8,1.8)
 
     start = time.time()
-    ## Set Resolution
-    ## Camera Version 1
-    # camera.resolution = (2592,1944)
-    ## Camera Version 2
-    camera.resolution = (3280,2464)
-    print("time to set resolution: ",time.time() - start);
 
     ## Set framerate and iso
     isoSet = time.time()
-    camera.framerate = 15 
+    camera.framerate = 10 
     camera.iso = 200
     # camera.start_preview()
-    print("time to set iso: ",time.time() - isoSet);
+    print("time to set iso: ",time.time() - isoSet)
+
+    ## Set Resolution
+    print("Camera Version: ",camera.revision)
+    ## Camera Version 1
+    camera.resolution = (2592,1944)
+    ## Camera Version 2
+    # camera.resolution = (3280,2464)
+    print("time to set resolution: ",time.time() - start)
 
     ## Wait for things to stabelize and then output shutter speed
+    camera.start_preview()
     sleep(2)
     baseExposure = camera.exposure_speed
     print(baseExposure)
@@ -56,13 +59,13 @@ with picamera.PiCamera() as camera:
     camera.capture('ldr_03.jpg')
     #0.0125s exposure
     # camera.framerate = 30
-    camera.shutter_speed = baseExposure/2
+    camera.shutter_speed = int(baseExposure/2)
     camera.capture('ldr_04.jpg')
     #0.003125s exposure 
-    camera.shutter_speed = baseExposure/4
-    camera.capture('ldr_05.jpg')
+    #camera.shutter_speed = int(baseExposure/4)
+    #camera.capture('ldr_05.jpg')
     #0.0008s exposure
-    #camera.shutter_speed = baseExposure/6
+    #camera.shutter_speed = int(baseExposure/6)
     #camera.capture('ldr_06.jpg')
     finish = time.time()
 print("Captured images in: ",(finish-startCatputer))
